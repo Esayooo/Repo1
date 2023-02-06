2dbcf6d54ac2eeb0bd2aadd38fd21645290d5977
Esayooo <yercanat@gmail.com>
2023-02-07 02:19:48 +0600

    Квадраттық функция құрдық

--- easy.java
+++ easy.java
@@ -1,5 +1,13 @@
 public class easy{
     public static void name(string[] args) {
         System.out.println("Hello world!");
+
+        int a=12;
+        int res;
+        res=kvadrat(a);
+        System.out.print(res);
+    }
+    public static int kvadrat(int a){
+        return a*a;
     }
 }
--- easy.py
+++ easy.py
@@ -1,5 +1,12 @@
 #hello world сөзін шығару
 print("Hello world")
 
+def kvadrat(a):
+    return a*a
+
+a=12
+res=kvadrat(a)
+print('a= ',res)
+
 def title():
     print("TITLE")
