5aa72ac574dc2b19f559db8ac3a46cfeb5043712
Esayooo <yercanat@gmail.com>
2023-02-07 08:33:08 +0600

    lab2.py файоға функция қосылды

--- lab2.py
+++ lab2.py
@@ -1,5 +1,17 @@
 name= input("What is your name?")
 phone =input("Enter your phone number:")
 print(name+phone)
+a = input("a:")
+b = input("b:")
+def bigger(a,b):
+    if a>b:
+        print("%s is bigger" %a)
+    elif a<b:
+        print("%s is bigger" %b)
+    else:
+        print("%s is equal to %s" %(a,b))
+    return 'The end'
+
+bigger(a,b)
 
 
