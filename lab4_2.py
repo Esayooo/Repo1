ac2525057a68ff17c01f4ebe45641f0cadcb92e9
Esayooo <yercanat@gmail.com>
2023-02-14 08:32:14 +0600

    lab4 2ші есеп өзгеріс енгіздік

--- lab4_2.py
+++ lab4_2.py
@@ -1,6 +1,6 @@
 a=input("Enter numer1:")
 b=input("Enter number2:")
-while not(a.isdigit() and b.isdigit()):
+while (a.isdigit()==False and b.isdigit()==False):
     a=input("Enter numer1:")
     b=input("Enter number2:")
 print(int(a)+int(b))
